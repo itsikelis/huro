47eef6b21611a965791a01626086e7c0c98b524e
sami <sami.leroux75@gmail.com>
2026-06-06 17:17:14 +0200

    tested: new heighmap in lidar frame

--- apps/rl_tasks/locomotion/go2/go2_publisher_cnn.py
+++ apps/rl_tasks/locomotion/go2/go2_publisher_cnn.py
@@ -94,7 +94,7 @@ class Go2PolicyController(Node):
         print(f"[INFO] Using device: {self.device}")
         
         # Load policy model        
-        policy_lidar_name = "policy_cnn_new1.pt"
+        policy_lidar_name = "policy_cnn_lidar_frame1.pt"
         policy_name = "policy_asymmetric_best.pt"
         policy_lidar_path = os.path.join(share, "resources", "models", "go2", policy_lidar_name)
         policy_path = os.path.join(share, "resources", "models", "go2", policy_name)
